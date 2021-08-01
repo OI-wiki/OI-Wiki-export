bd784d895f575316f097b3d3c40ef565d01a5107
mgt <mgt@oi-wiki.org>
2021-08-01 20:10:22 +0800

    refactor: reformat code

--- remark-latex/test.js
+++ remark-latex/test.js
@@ -11,35 +11,35 @@ const vfile = require('to-vfile')
 const path = require('path')
 const child_process = require('child_process')
 
-let filename = './example.md'
+const filename = './example.md'
 const prefixRegEx = /[^a-zA-Z0-9]/ig
 
 if (!fs.existsSync(filename)) {
-	console.log('Error: File \'' + filename + '\' does not exist')
-	process.exit()
+  console.log('Error: File \'' + filename + '\' does not exist')
+  process.exit()
 }
 
 try {
-	fs.mkdirSync('images')
+  fs.mkdirSync('images')
 } catch (e) {}
 unified()
-	.use(parse)
-	.use(math)
-	.use(details)
-	.use(footnotes)
-	.use(latex, {
-		prefix: filename.replace(prefixRegEx, "").replace(/md$/, ""),
-		depth: 0,
-		current: filename,
-		root: path.join('./', 'docs'),
-		nested: false,
-		forceEscape: false,
-		path: filename.replace(/\.md$/, "/").split('/').slice(0, -2).join('/')
-	})
-	.process(vfile.readSync(filename), function (err, file) {
-		if (err) {
-			throw err
-		}
-		file.extname = '.tex'
-		vfile.writeSync(file)
-	})
+  .use(parse)
+  .use(math)
+  .use(details)
+  .use(footnotes)
+  .use(latex, {
+    prefix: filename.replace(prefixRegEx, '').replace(/md$/, ''),
+    depth: 0,
+    current: filename,
+    root: path.join('./', 'docs'),
+    nested: false,
+    forceEscape: false,
+    path: filename.replace(/\.md$/, '/').split('/').slice(0, -2).join('/')
+  })
+  .process(vfile.readSync(filename), function (err, file) {
+    if (err) {
+      throw err
+    }
+    file.extname = '.tex'
+    vfile.writeSync(file)
+  })
