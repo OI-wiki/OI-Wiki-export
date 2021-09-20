16019dae74c5bc0c5cb69675c0dac605fbbc2231
Alisa <wuminyan0607@gmail.com>
2021-09-20 17:13:43 +0800

    add run.py

---
+++ export-epub/run.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import yaml
+
+
+path = ""
+
+def getTitle(title):
+    title = title.replace(" ","")
+    title = title.replace("/","")
+    title = title.replace("&","")
+    title = title.replace("-","")
+    title = title.replace("|","")
+    title = title.replace("(","（")
+    title = title.replace(")","）")
+    
+    return title
+
+def dfs(root, title):
+    if(isinstance(root, list)):
+        for x in root:
+            dfs(x, "")
+    elif (isinstance(root, dict)):
+        for k,v in root.items():
+            dfs(v, k)
+            if(isinstance(v, list)):
+                # print(k)
+                title = getTitle(k)
+                # print("title = ", title)
+                order = "python3 ./EpubMerge/epubmerge.py --title=" + title + " "
+                for pair in v:
+                    subTitle = getTitle(next(iter(pair)))
+                    subValue = pair[next(iter(pair))]
+                    if(isinstance(subValue, list)):
+                        dest_file = path + "./tmpdir/" + subTitle + ".epub"
+                        order = order + dest_file + " "
+                    else:
+                        str = path +"./docs/"+subValue
+                        slash_pos = str.rfind("/")
+                        resource_path = str[:slash_pos + 1]
+                        dest_file =  resource_path + subTitle + ".epub"
+                        order = order + dest_file + " "
+                order = order + "-o " + path + "./tmpdir/" + title + ".epub"
+                # print(order)
+                os.system(order)
+
+    else:
+        str = path + "./docs/"+root
+        slash_pos = str.rfind("/")
+        title = getTitle(title)
+        resource_path = str[:slash_pos + 1]
+        dest_file =  resource_path + title + '.epub'
+
+        order = "pandoc --webtex=http://127.0.0.1:8888/svg.latex? --self-contained --resource-path "+ resource_path +" --metadata title=" + title + " -f markdown+tex_math_dollars "+ str +" -o " + dest_file
+
+        # print(order)
+        os.system(order)
+        
+
+print(len(sys.argv))
+if(len(sys.argv) == 2):
+    path = sys.argv[1] 
+print(path)
+os.system("mkdir " + path + "tmpdir")
+f = open(path+"mkdocs.yml")
+# print (os.getcwd())
+y = yaml.safe_load(f)
+nav = y['nav']
+# print(nav)
+dfs(nav, "")
+order = "python3 " + path + "./EpubMerge/epubmerge.py --title=" + "OI-Wiki" + " "
+for pair in nav:
+    # print(pair)
+    subTitle = getTitle(next(iter(pair)))
+    dest_file = path + "./tmpdir/" + subTitle + ".epub"
+    order = order + dest_file + " "
+order = order + "-o " + path + "./tmpdir/OI-Wiki.epub"
+print(order)
+os.system(order)
+os.system("cp " + path + "tmpdir/OI-Wiki.epub ./OI-Wiki.epub")
