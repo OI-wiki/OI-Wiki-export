9d4cd2179b24f9b75c0418ca803a37bc36f097b5
megakite <hello@megakite.icu>
2023-09-22 20:07:35 +0800

    fix: wrong starting directory, problematic links in docs

---
+++ oi-wiki-export/patch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sed -i 's/intro/contest/g' ../OI-wiki/docs/lang/pas-cpp.md
+sed -i '3s#.*#![./images/kuaizhuanglianbiao.png](./images/kuaizhuanglianbiao.png "./images/kuaizhuanglianbiao.png")#' ../Oi-wiki/docs/ds/block-list.md
