e9b8a0be23400f177bbc03c9811a605c75fca33e
megakite <hello@megakite.icu>
2023-09-22 19:08:53 +0800

    chore: remove unnecessary patch & test

--- oi-wiki-export/svg-test.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-import { renderSvg } from '../remark-typst/remark-mathjax/svg.js'
-
-let node = { value: 'e^{i\\pi}+1=0' }
-renderSvg({}, { scale: .25, fontCache: 'global' }).render(node, {})
-
-console.log(node.value)
