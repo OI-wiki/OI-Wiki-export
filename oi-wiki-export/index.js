8d5dcc82c164032817987479c65c1663bb9949f7
Shawlley <wsyy0619@gmail.com>
2023-10-25 17:12:44 +0800

    feat(latex): support pymdown tabbed syntax by flattening content in tabs

--- oi-wiki-export/index.js
+++ oi-wiki-export/index.js
@@ -1,146 +1,167 @@
-'use strict'
+"use strict";
 
-const unified = require('unified')
-const parse = require('remark-parse')
-const math = require('remark-math')
-const details = require('remark-details')
-const footnotes = require('remark-footnotes')
-const latex = require('remark-latex')
-const fs = require('fs').promises
-const vfile = require('to-vfile')
-const path = require('path')
-const yaml = require('js-yaml')
-const escape = require('escape-latex')
-const child_process = require('child_process')
-const snippet = require('./snippet')
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkMath from "remark-math";
+import remarkDetails from "remark-details";
+import remarkGfm from "remark-gfm";
+import { latex } from "remark-latex";
+import remarkFootnotes from "remark-footnotes";
+import { read, writeSync } from "to-vfile";
+import { Type, Schema, load } from "js-yaml";
+import { join } from "path";
+import { promises as fs } from "fs";
+import { snippet } from "./snippet.js";
+import remarkTabbed from "remark-tabbed";
+import escape from "escape-latex";
 
-const prefixRegEx = /[^a-zA-Z0-9]/ig
+const prefixRegEx = /[^a-zA-Z0-9]/gi;
 
-async function exists (file) {
+async function exists(file) {
   try {
-    await fs.access(file)
+    await fs.access(file);
   } catch (e) {
-    return false
+    return false;
   }
-  return true
+  return true;
 }
 
-async function main () {
+async function main() {
   if (process.argv.length !== 3) {
-    console.log('Usage: node ' + __filename.split('/').pop() + ' <oi-wiki-root>')
-    process.exit()
+    console.log(
+      "Usage: node " + __filename.split("/").pop() + " <oi-wiki-root>"
+    );
+    process.exit();
   }
 
-  const oiwikiRoot = process.argv[2] // OI Wiki 根目录
-  const yamlFile = path.join(oiwikiRoot, 'mkdocs.yml') // YAML 配置文件
+  const oiwikiRoot = process.argv[2]; // OI Wiki 根目录
+  const yamlFile = join(oiwikiRoot, "mkdocs.yml"); // YAML 配置文件
 
-  console.log('Processing snippets')
+  console.log("Processing snippets");
 
-  await snippet.snippet(oiwikiRoot)
+  await snippet(oiwikiRoot);
 
-  console.log('Checking for tex/ directory')
+  console.log("Checking for tex/ directory");
 
   try {
-    await fs.mkdir('tex')
-    await fs.mkdir('images')
-  } catch (e) {
+    await fs.mkdir("tex");
+    await fs.mkdir("images");
+  } catch (e) {}
+  console.log("[Info] Exporting OI Wiki from directory: " + oiwikiRoot);
 
-  }
-  console.log('[Info] Exporting OI Wiki from directory: ' + oiwikiRoot)
-
-  if (!await exists(yamlFile)) {
-    console.log('Error: config file \'mkdocs.yml\' does not exist')
-    process.exit()
+  if (!(await exists(yamlFile))) {
+    console.log("Error: config file 'mkdocs.yml' does not exist");
+    process.exit();
   }
 
-  console.log('Config file: ' + yamlFile)
+  console.log("Config file: " + yamlFile);
 
-  const yamlFileContent = await fs.readFile(yamlFile, 'utf8');
+  const yamlFileContent = await fs.readFile(yamlFile, "utf8");
 
   // fix YAMLException: unknown tag
   const types = yamlFileContent.match(/!!python\/name:.*/g).map(
-      s =>
-          new yaml.Type(s.replace('!!', 'tag:yaml.org,2002:'), {
-            kind: 'mapping',
-            construct: function (data) {
-              return data
-            }
-          })
-  )
-  const CONFIG_SCHEMA = yaml.Schema.create(types)
+    (s) =>
+      new Type(s.replace("!!", "tag:yaml.org,2002:"), {
+        kind: "mapping",
+        construct: function (data) {
+          return data;
+        },
+      })
+  );
+  const CONFIG_SCHEMA = new Schema(types);
 
-  const config = yaml.load(yamlFileContent, { schema: CONFIG_SCHEMA })
-  const catalog = config.nav // 文档目录
+  const config = load(yamlFileContent, { schema: CONFIG_SCHEMA });
+  const catalog = config.nav; // 文档目录
 
-  let includes = ''
+  let includes = "";
   for (const id in catalog) {
-    const texModule = path.join('tex', id.toString())
-    await fs.writeFile(texModule + '.tex', await exportRecursive(catalog[id], 0))
-    includes += '\\input{' + texModule + '}\n' // 输出 includes.tex 章节目录文件
+    const texModule = join("tex", id.toString());
+    await fs.writeFile(
+      texModule + ".tex",
+      await exportRecursive(catalog[id], 0)
+    );
+    includes += "\\input{" + texModule + "}\n"; // 输出 includes.tex 章节目录文件
   }
 
-  await fs.writeFile('includes.tex', includes)
-  console.log('Complete')
+  await fs.writeFile("includes.tex", includes);
+  console.log("Complete");
 
-  async function convertMarkdown (filename, depth) {
-    if (!filename.endsWith('.md')) {
-      console.log('Error: File \'' + filename + '\' is not a markdown file')
-      process.exit()
+  async function convertMarkdown(filename, depth) {
+    if (!filename.endsWith(".md")) {
+      console.log("Error: File '" + filename + "' is not a markdown file");
+      process.exit();
     }
 
-    if (!await exists(filename)) {
-      console.log('Error: File \'' + filename + '\' does not exist')
-      process.exit()
+    if (!(await exists(filename))) {
+      console.log("Error: File '" + filename + "' does not exist");
+      process.exit();
     }
 
     unified()
-      .use(parse)
-      .use(math)
-      .use(details)
-      .use(footnotes)
+      .use(remarkParse)
+      .use(remarkMath)
+      .use(remarkGfm)
+      .use(remarkDetails)
+      .use(remarkTabbed)
+      .use(remarkFootnotes)
       .use(latex, {
-        prefix: filename.replace(prefixRegEx, '').replace(/md$/, ''), // 根据路径生成 ID，用作 LaTeX label
+        prefix: filename.replace(prefixRegEx, "").replace(/md$/, ""), // 根据路径生成 ID，用作 LaTeX label
         depth: depth, // 标题 h1 深度
         current: filename, // 带 md 后缀的文件名
-        root: path.join(oiwikiRoot, 'docs'), // docs/ 目录
+        root: join(oiwikiRoot, "docs"), // docs/ 目录
         nested: false,
         forceEscape: false,
-        path: filename.replace(/\.md$/, '/') // 由文件名转换而来的路径
+        path: filename.replace(/\.md$/, "/"), // 由文件名转换而来的路径
       })
-      .process(await vfile.read(filename), function (err, file) {
+      .process(await read(filename), function (err, file) {
         if (err) {
-          throw err
+          throw err;
         }
-        file.dirname = 'tex'
-        file.stem = filename.replace(prefixRegEx, '')
-        file.extname = '.tex'
-        vfile.writeSync(file)
-      })
+        file.dirname = "tex";
+        file.stem = filename.replace(prefixRegEx, "");
+        file.extname = ".tex";
+        writeSync(file);
+      });
   }
 
   // 递归处理各个 chapter
-  async function exportRecursive (object, depth) {
-    const block = ['chapter', 'section', 'subsection', 'subsubsection', 'paragraph', 'subparagraph'] // 各层次对应的 TeX 命令
-    let result = ''
-    depth = Math.min(depth, block.length)
+  async function exportRecursive(object, depth) {
+    const block = [
+      "chapter",
+      "section",
+      "subsection",
+      "subsubsection",
+      "paragraph",
+      "subparagraph",
+    ]; // 各层次对应的 TeX 命令
+    let result = "";
+    depth = Math.min(depth, block.length);
     for (const key in object) {
-      console.log('[Info] Exporting: ' + key)
-      result += '\\' + block[depth] + '{' + escape(key) + '}\n'
-      if (typeof object[key] === 'string') { // 对应页面
-        await convertMarkdown(path.join(oiwikiRoot, 'docs', object[key]), depth + 1)
-        result += '\\input{' + escape(getTexModuleName(object[key])) + '}\n'
-      } else { // 对应子目录
+      console.log("[Info] Exporting: " + key);
+      result += "\\" + block[depth] + "{" + escape(key) + "}\n";
+      if (typeof object[key] === "string") {
+        // 对应页面
+        await convertMarkdown(
+          join(oiwikiRoot, "docs", object[key]),
+          depth + 1
+        );
+        result += "\\input{" + escape(getTexModuleName(object[key])) + "}\n";
+      } else {
+        // 对应子目录
         for (const id in object[key]) {
-          result += await exportRecursive(object[key][id], depth + 1)
+          result += await exportRecursive(object[key][id], depth + 1);
         }
       }
     }
-    return result
+    return result;
   }
 
-  function getTexModuleName (name) {
-    return path.join('tex', path.join(oiwikiRoot, 'docs', name).replace(prefixRegEx, ''))
+  function getTexModuleName(name) {
+    return join(
+      "tex",
+      join(oiwikiRoot, "docs", name).replace(prefixRegEx, "")
+    );
   }
 }
 
-main()
+main();
--- oi-wiki-export/snippet.js
+++ oi-wiki-export/snippet.js
@@ -1,42 +1,47 @@
-const glob = require('tiny-glob')
-const fs = require('fs').promises
-const path = require('path')
-const SNIPPET_TOKEN = '--8<-- '
+import glob from "tiny-glob";
+import { promises as fs } from "fs";
+import { resolve } from "path";
+const SNIPPET_TOKEN = "--8<-- ";
 
-let oi_wiki_root = '.'
+let oi_wiki_root = ".";
 
-function resolvePath (snip, spacesAtStart) {
-  let str = snip.substring(SNIPPET_TOKEN.length + spacesAtStart)
-  if ((str.startsWith('"') && str.endsWith('"')) ||
-    (str.startsWith("'") && str.endsWith("'"))) {
-    str = str.substring(1, str.length - 1)
+function resolvePath(snip, spacesAtStart) {
+  let str = snip.substring(SNIPPET_TOKEN.length + spacesAtStart);
+  if (
+    (str.startsWith('"') && str.endsWith('"')) ||
+    (str.startsWith("'") && str.endsWith("'"))
+  ) {
+    str = str.substring(1, str.length - 1);
   } else {
-    console.error('cannot parse snippet:', snip)
+    console.error("cannot parse snippet:", snip);
   }
-  return path.resolve(oi_wiki_root, str)
+  return resolve(oi_wiki_root, str);
 }
 
-async function process_snippet (file) {
-  const content = await fs.readFile(file, 'utf8')
-  const res = (await Promise.all(content
-    .split('\n')
-    .map(async (line) => {
-      const spacesAtStart = line.length - line.trimLeft().length
-      const spaceString = ' '.repeat(spacesAtStart)
-      if (line.trim().startsWith(SNIPPET_TOKEN)) {
-        const res = resolvePath(line, spacesAtStart)
-        line = await fs.readFile(res, 'utf8')
-        line = line.split('\n').map(l => spaceString + l).join('\n')
-      }
-      return line
-    })))
-    .join('\n')
-  await fs.writeFile(file, res)
+async function process_snippet(file) {
+  const content = await fs.readFile(file, "utf8");
+  const res = (
+    await Promise.all(
+      content.split("\n").map(async (line) => {
+        const spacesAtStart = line.length - line.trimLeft().length;
+        const spaceString = " ".repeat(spacesAtStart);
+        if (line.trim().startsWith(SNIPPET_TOKEN)) {
+          const res = resolvePath(line, spacesAtStart);
+          line = await fs.readFile(res, "utf8");
+          line = line
+            .split("\n")
+            .map((l) => spaceString + l)
+            .join("\n");
+        }
+        return line;
+      })
+    )
+  ).join("\n");
+  await fs.writeFile(file, res);
 }
 
-module.exports = {}
-module.exports.snippet = async function snippet (root) {
-  oi_wiki_root = root
-  const files = await glob(`${path.resolve(root)}/**/*.md`)
-  await Promise.all(files.map(process_snippet))
+export async function snippet(root) {
+  oi_wiki_root = root;
+  const files = await glob(`${resolve(root)}/**/*.md`);
+  await Promise.all(files.map(process_snippet));
 }
