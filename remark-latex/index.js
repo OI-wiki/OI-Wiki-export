587629adfe1d0069a63617551ff145e63153cb71
Shaoyu Wang <wsyy0619@gmail.com>
2023-10-26 01:51:55 +0800

    为latex导出添加tabbed语法支持 (#51)
    * feat(latex): support pymdown tabbed syntax by flattening content in tabs
    
    * fix(ci): change npm cache dir

--- remark-latex/index.js
+++ remark-latex/index.js
@@ -1,9 +1,7 @@
 'use strict'
 
-const compiler = require('./lib/compiler')
+import compiler from "./lib/compiler.js"
 
-module.exports = latex
-
-function latex (options) {
+export function latex (options) {
   this.Compiler = compiler(options || {})
 }
